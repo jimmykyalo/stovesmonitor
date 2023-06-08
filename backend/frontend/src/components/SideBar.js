7f340376d57040f5d4da29a5b1cff044f841b940
Jimmy Kyalo <jimm.kyalo@gmail.com>
2023-06-08 10:08:50 +0300

    manual operations

--- backend/frontend/src/components/SideBar.js
+++ backend/frontend/src/components/SideBar.js
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Sidebar, Menu, MenuItem, sidebarClasses, menuClasses, SubMenu } from 'react-pro-sidebar';
 import { ImStatsDots } from 'react-icons/im';
 import { GoProject } from 'react-icons/go';
 import { GiBrickPile, GiCampCookingPot, GiThrownCharcoal, GiWoodPile } from 'react-icons/gi';
 import { TbReport } from 'react-icons/tb';
 import { RiPlug2Line } from 'react-icons/ri';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProjects } from '../actions/projectActions';
+import { FaList } from 'react-icons/fa';
+import { FcApproval } from 'react-icons/fc';
+
 
 function SideBar() {
+    const navigate = useNavigate()
+  	const dispatch = useDispatch()
+    const userLogin = useSelector(state=> state.userLogin)
+	const {userInfo} = userLogin
+
+    const projectList = useSelector(state=> state.projectList)
+	const {error:errorProjects, loading:loadingProjects, projects} = projectList
+
+    useEffect(() => {
+        if(!userInfo){
+            navigate('/')
+        }else{
+			projects.length===0 && dispatch(listProjects())
+		}
+    }, [userInfo])
+    
   return (
     <Sidebar rootStyles={{
         [`.${sidebarClasses.container}`]: {
@@ -25,10 +47,11 @@ function SideBar() {
                     },
                     },
                 }}
+                component={<Link to={'/home'} />}
             >
                 Dashboard
             </MenuItem>
-            <MenuItem 
+            {/* <MenuItem 
                 icon={<GoProject />} 
                 rootStyles={{
                     ['.' + menuClasses.button]: {
@@ -39,12 +62,13 @@ function SideBar() {
                     },
                     },
                 }}
+                component={<Link to="/projects" />}
             >
                 Projects
-            </MenuItem>
+            </MenuItem> */}
             <SubMenu
-                label="Stoves"
-                icon={<GiCampCookingPot />} 
+                label="Projects"
+                icon={<GoProject />} 
                 rootStyles={{
                     ['.' + menuClasses.button]: {
                     backgroundColor: 'transparent',
@@ -56,13 +80,18 @@ function SideBar() {
                     ['.' + menuClasses.subMenuContent]: {
                         backgroundColor: '#0bab64',
                     },
+
         
                 }}
+                
             >
-                <MenuItem icon={<RiPlug2Line />}> Electric</MenuItem>
+                {/* <MenuItem icon={<RiPlug2Line />}> Electric</MenuItem>
                 <MenuItem icon={<GiWoodPile />}>Firewood</MenuItem>
                 <MenuItem icon={<GiThrownCharcoal />}>Charcoal</MenuItem>
-                <MenuItem icon={<GiBrickPile />}>Brickets</MenuItem>
+                <MenuItem icon={<GiBrickPile />}>Brickets</MenuItem> */}
+                <MenuItem component={<Link to={`projects`} />} icon={<FaList />}>All Projects</MenuItem>
+                {(!loadingProjects && !errorProjects) && projects.map(p=>
+                    <MenuItem key={p._id} component={<Link to={`project/?id=${p._id}`} />} icon={<FcApproval />}>{p.name}</MenuItem>)}
             </SubMenu>
             
             <MenuItem 
